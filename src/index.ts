1f87b6ac46c637ae10e36ade3f2580b56ab71dcc
kross <cristemperatum@gmail.com>
2021-12-17 10:16:11 -0600

    hacker rank 4

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 /* eslint-disable no-return-assign */
 
 // Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
-
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function diagonalDifference(arr: number[][]): number {
+  const longitud = arr.length;
+  let diago1 = 0;
+  let diago2 = 0;
+  for (let i = 0; i < longitud; i++) {
+    for (let j = 0; j < longitud; j++) {
+      if(i === j){
+        diago1=diago1+arr[i][j]
+      }
+    }
+  }
+  for (let i = 0; i < longitud; i++) {
+    for (let j = 0; j < longitud; j++) {
+      if(i + j === longitud-1){
+        diago2=diago2+arr[i][j]
+      }
+    }
+  }
+  
+  return Math.abs(diago2-diago1)
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const a = [[11,2,4],[4,5,6],[10,8,-12]]
+  const result = diagonalDifference(a);
   console.log('Result: ', result);
 }
 
